e674801646366498f1b008194c93b0217694b463
massaraksh111 <massaraksh111@gmail.com>
2014-06-24 15:45:44 +0400

    Added params for clearing rrl cache. Added params in rec_control for reloading files white_list.conf and special_limits.conf

--- pdns/rrliptable.hh
+++ pdns/rrliptable.hh
@@ -1,16 +1,20 @@
 #ifndef RRLIPTABLE_H
 #define RRLIPTABLE_H
 
-#include <list>
+#include <set>
 #include <map>
 
 #include <boost/date_time/posix_time/posix_time.hpp>
 
 #include "iputils.hh"
-#include "qtype.hh"
-#include "logger.hh"
+//#include "qtype.hh"
+//#include "logger.hh"
 
 struct RrlNode;
+
+class QType;
+class Logger;
+class Netmask;
 
 struct RrlSingleLimit
 {
@@ -26,9 +30,16 @@ struct RrlSingleLimit
   u_int32_t blocking_time;
 };
 
-class RrlIpTable
+class RrlIpTable : public boost::noncopyable
 {
   typedef boost::posix_time::ptime RrlTime;
+
+  enum CleaningMode
+  {
+      None,
+      LargerThan,
+      RemoveOld
+  };
 
   struct Node
   {
@@ -41,20 +52,35 @@ class RrlIpTable
      Node() : block_till(), last_request_time(), counter_ratio(0),
        counter_types(0), blocked(false)
      { }
-  };
+  };  
 
 public:
   typedef std::map<Netmask, Node> Map;
 
 private:
 
+  struct SortRrlNodes : std::binary_function<Map::iterator, Map::iterator, bool>
+  {
+      bool operator()(Map::iterator first, Map::iterator second) const
+      { return first->second.last_request_time < second->second.last_request_time; }
+  };
+
   u_int8_t  d_ipv4_prefix_length;
   u_int8_t  d_ipv6_prefix_length;
   bool      d_drop_queries;
   bool      d_enabled;
 
+  CleaningMode  d_clean_mode;
+  u_int32_t     d_clean_remove_every_n_request;
+  u_int32_t     d_clean_remove_if_older;
+  u_int32_t     d_clean_remove_if_larger;
+  u_int32_t     d_clean_remove_n_nodes;
+  u_int32_t     d_request_counter;
+
   std::vector<RrlSingleLimit> d_limits;
+  bool              d_limits_enabled;
   std::set<Netmask> d_white_list;
+  bool              d_white_list_enabled;
   Map               d_data;
   Logger*           d_logger;
 
@@ -64,22 +90,29 @@ private:
   int findLimitIndex(const ComboAddress &address);
 
   void parseRequestTypes(const string& str, std::set<QType>& types);
-  void parseLimitFile(const string& filename);
-  void parseWhiteList(const string& filename);
+  void cleanRrlNodes();
+  string parseLimitFile(const string& filename);
+  string parseWhiteList(const string& filename);
 
-  Logger& log() { return d_logger ? *d_logger : theL(); }
+  void initCleaningMode();
+  RrlSingleLimit initDefaulLimits();
+
+  Logger& log();
 
 public:
   RrlIpTable();
-  ~RrlIpTable() { delete d_logger; }
+  ~RrlIpTable();
 
-  RrlNode getNode(const ComboAddress& addr);
+  RrlNode getNode(const ComboAddress& addr, bool tryToClean = true);
   bool decreaseCounters(RrlNode node);
 
   bool updateRecord(RrlNode rrlNode, QType type);
   bool updateRecord(RrlNode rrlNode, double ratio);
 
   bool dropQueries() const { return d_drop_queries; }
+
+  string reloadWhiteList();
+  string reloadSpecialLimits();
 };
 
 struct RrlNode
